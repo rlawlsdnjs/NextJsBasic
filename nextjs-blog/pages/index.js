d4359f1dc629fb2398eaebaba1e613bd79546867
JinWon <dlfjs9514@nate.com>
2023-05-21 23:48:44 +0900

    SSG 동작

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -3,26 +3,60 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import Alert from "../components/alert";
 import Link from "next/link";
-export default function Home() {
+
+import { getSortedPostsData } from "../lib/post";
+
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData,
+        },
+    };
+}
+
+export default function Home({ allPostsData }) {
     return (
         <Layout home>
-            <Head>
-                <title>{siteTitle}</title>
-            </Head>
-            <h1 className="title">
-                Read <Link href="/posts/first-posts">this page!</Link>
-            </h1>
+            {/* Keep the existing code here */}
 
-            <section className={utilStyles.headingMd}>
-                <p>[Your Self Introduction]</p>
-                <p>
-                    (This is a sample website - you’ll be building a site like
-                    this on{" "}
-                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>
-                    .)
-                </p>
+            {/* Add this <section> tag below the existing <section> tag */}
+            <section
+                className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            {title}
+                            <br />
+                            {id}
+                            <br />
+                            {date}
+                        </li>
+                    ))}
+                </ul>
             </section>
-            <Alert />
         </Layout>
     );
+    // return (
+    //     <Layout home>
+    //         <Head>
+    //             <title>{siteTitle}</title>
+    //         </Head>
+    //         <h1 className="title">
+    //             Read <Link href="/posts/first-posts">this page!</Link>
+    //         </h1>
+
+    //         <section className={utilStyles.headingMd}>
+    //             <p>[Your Self Introduction]</p>
+    //             <p>
+    //                 (This is a sample website - you’ll be building a site like
+    //                 this on{" "}
+    //                 <a href="https://nextjs.org/learn">our Next.js tutorial</a>
+    //                 .)
+    //             </p>
+    //         </section>
+    //         <Alert />
+    //     </Layout>
+    // );
 }
