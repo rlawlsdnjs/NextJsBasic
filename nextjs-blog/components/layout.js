342fc50a4d0695700a3addf8e8a984a5511ed16e
JinWon <dlfjs9514@nate.com>
2023-06-24 06:46:57 +0900

    post page CSS 적용

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Your Name";
+const name = "Jin Won Kim";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
