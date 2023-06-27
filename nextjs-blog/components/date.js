3a56c374de7a30d72d5fe2b200e22912ff9da321
JinWon <dlfjs9514@nate.com>
2023-06-28 07:40:40 +0900

    index page edit, time error 해결

--- nextjs-blog/components/date.js
+++ nextjs-blog/components/date.js
@@ -1,6 +1,12 @@
-import { parseISO, format } from "date-fns";
+import { parseISO, format, isValid } from "date-fns";
 
 export default function Date({ dateString }) {
     const date = parseISO(dateString);
+
+    // 유효한 날짜인지 확인
+    if (!isValid(date)) {
+        return null;
+    }
+
     return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
 }
--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -3,7 +3,7 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import Alert from "../components/alert";
 import Link from "next/link";
-
+import Date from "../components/date";
 import { getSortedPostsData } from "../lib/post";
 
 export async function getStaticProps() {
@@ -27,11 +27,11 @@ export default function Home({ allPostsData }) {
                 <ul className={utilStyles.list}>
                     {allPostsData.map(({ id, date, title }) => (
                         <li className={utilStyles.listItem} key={id}>
-                            {title}
+                            <Link href={`/posts/${id}`}>{title}</Link>
                             <br />
-                            {id}
-                            <br />
-                            {date}
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
                         </li>
                     ))}
                 </ul>
