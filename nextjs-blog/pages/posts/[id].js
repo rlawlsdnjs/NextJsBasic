0c8d0b3297de3fff46786470ae45f85d6a129e79
JinWon <dlfjs9514@nate.com>
2023-06-28 08:08:08 +0900

    vercel git igno

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -23,19 +23,19 @@ export default function Post({ postData }) {
     );
 }
 
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
 export async function getStaticProps({ params }) {
     const postData = await getPostData(params.id);
     return {
         props: {
             postData,
         },
-    };
-}
-
-export async function getStaticPaths() {
-    const paths = getAllPostIds();
-    return {
-        paths,
-        fallback: false,
     };
 }
