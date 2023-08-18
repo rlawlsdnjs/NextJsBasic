f4585285d12705c79c41628fc7a8a309ee590cd3
JinWon <dlfjs9514@nate.com>
2023-08-19 00:32:50 +0900

    no message

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "../lib/post";
+import { getSortedPostsData } from "../lib/post.js";
 import Link from "next/link";
 import Date from "../components/date";
 
